4ff9aa245de3badc777677af3190bacddf99edae
nuiYun <102241153+nuiYun@users.noreply.github.com>
2022-03-24 11:12:29 -0500

    Create data.go

---
+++ data.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "math/rand"
+  
+  "github.com/hajimehoshi/ebiten"
+  "github.com/jonasbabadilla/bombit"
+
+
+)
+
+type Game struct{
+  char_aliv int
+  time int
+  
+  
+}
+
+type Char interface{
+  
+  
+  
+func Check(g Game) bool {
+  
+  
+  
+  
+}
+
+func 
