92726fec2ef385f56761050d129cd17ba997b415
jonasbabadilla <101443822+jonasbabadilla@users.noreply.github.com>
2022-03-24 10:06:18 -0500

    Title Change

---
+++ backup.go
@@ -0,0 +1,81 @@
+// Copyright 2014 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build example
+// +build example
+
+package main
+
+import (
+	"bytes"
+	"image"
+	_ "image/png"
+	"log"
+
+	//"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/jonasbabadilla/bombit/imageResources"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+type Game struct {
+}
+
+var (
+	button *ebiten.Image
+)
+
+func (g *Game) Update() error {
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	w, h := button.Size()
+	op := &ebiten.DrawImageOptions{}
+
+	// Move the image's center to the screen's upper-left corner.
+	// This is a preparation for rotating. When geometry matrices are applied,
+	// the origin point is the upper-left corner.
+	op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
+
+	// Move the image to the screen's center.
+	op.GeoM.Translate(screenWidth/2, screenHeight/2)
+
+	screen.DrawImage(button, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+
+	img, _, err := image.Decode(bytes.NewReader(imageResources.StartButton_png))
+	if err != nil {
+		log.Fatal(err)
+	}
+	button = ebiten.NewImageFromImage(img)
+
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Bomb It")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
