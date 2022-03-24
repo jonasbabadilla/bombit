c8fbcf1aee0087a12bef6717d716e8377fa2f9a8
nuiYun <102241153+nuiYun@users.noreply.github.com>
2022-03-24 15:05:17 -0500

    Update titleScreen.go

--- Game/Bomb/titleScreen.go
+++ Game/Bomb/titleScreen.go
@@ -3,10 +3,13 @@ package bombIt
 import (
 	_ "image/png"
 
+	"fmt"
 	//"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
+
+var g *Game
 
 const (
 	screenWidth  = 640
@@ -18,19 +21,23 @@ type Game struct {
 }
 
 func NewGame() (*Game, error) {
-	g := &Game{
+	g = &Game{
 		input: mouseInput(),
 	}
+	
 	return g, nil
 }
 
 func (g *Game) Update() error {
-
+	g.input.Update()
+	if g.input.pressed == true {
+		fmt.Println("Pressed", g.input.mouseX, g.input.mouseY)
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-
+	
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
