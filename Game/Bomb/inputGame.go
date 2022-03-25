db6cb270f0cddf7752887787be974cb9af2b1514
nuiYun <102241153+nuiYun@users.noreply.github.com>
2022-03-25 10:04:32 -0500

    Added keyboard input

--- Game/Bomb/inputGame.go
+++ Game/Bomb/inputGame.go
@@ -10,22 +10,39 @@ import (
 // boom
 
 type InputGame struct {
-	pressed bool
-	mouseX int
-	mouseY int
+	DirUp    bool
+	DirDown  bool
+	DirLeft  bool
+	DirRight bool
 }
 
-func mouseInput() *InputGame {
+func keyInput() *InputGame {
+
 	return &InputGame{}
 }
 
-
 func (g *InputGame) Update() error {
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		g.pressed = true
-		g.mouseX, g.mouseY = ebiten.CursorPosition()
+
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		g.DirUp = true
 	} else {
-		g.pressed = false
+		g.DirUp = false
 	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		g.DirDown = true
+	} else {
+		g.DirDown = false
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		g.DirLeft = true
+	} else {
+		g.DirLeft = false
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		g.DirRight = true
+	} else {
+		g.DirRight = false
+	}
+
 	return nil
 }
