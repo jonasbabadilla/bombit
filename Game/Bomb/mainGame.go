39dc990a50c151909c008594769e4260c715f7bb
nuiYun <102241153+nuiYun@users.noreply.github.com>
2022-03-25 14:29:26 -0500

    Update mainGame.go

--- Game/Bomb/mainGame.go
+++ Game/Bomb/mainGame.go
@@ -32,7 +32,8 @@ func NewGame() (*Game, error) {
 	g = &Game{
 		input: keyInput(),
 	}
-
+	options = &ebiten.DrawImageOptions{}
+	options.GeoM.Translate(screenWidth/2, screenHeight/2)
 	return g, nil
 }
 
@@ -43,10 +44,12 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
-	x, y := g.input.DirUp.x, g.input.DirUp.y
-	options = &ebiten.DrawImageOptions{}
-
-	options.GeoM.Translate(float64(-x), float64(-y))
+	var x, y float64
+	x += float64(g.input.DirLeft+g.input.DirRight) 
+	y += float64(g.input.DirUp+g.input.DirDown)
+	
+	options.GeoM.Translate(x, y)
+	
 	screen.DrawImage(character, options)
 }
 
@@ -55,6 +58,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	img, _, _ := image.Decode(bytes.NewReader(imageResources.StartButton_png))
 
 	character = ebiten.NewImageFromImage(img)
+	
 
 	return screenWidth, screenHeight
 }
